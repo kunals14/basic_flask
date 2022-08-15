61ec951007a0df41bc1104ff12e7e116302f009e
“KunalSolanki14” <kunal.solanki@corecotechnologies.com>
2022-08-15 14:33:06 +0530

    added gitpython and git_update webhook

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, request
+import git
  
 # Flask constructor takes the name of
 # current module (__name__) as argument.
 app = Flask(__name__)
  
+
+@app.route('/git_update', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('./basic_flask')
+        origin = repo.remotes.origin
+        repo.create_head('master', 
+    origin.refs.master).set_tracking_branch(origin.refs.master).checkout()
+        origin.pull()
+        return '', 200
+    else:
+        return '', 400
+
 # The route() function of the Flask class is a decorator,
 # which tells the application which URL should call
 # the associated function.
